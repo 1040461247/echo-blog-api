215cf4209e8fbcfdb9a33ecc0551c5ff4e760d9b
田若茜 <1040461147@qq.com>
2024-05-20 13:56:02 +0800

    update

--- src/app/index.ts
+++ src/app/index.ts
@@ -3,12 +3,20 @@ import cors from '@koa/cors'
 import bodyparser from 'koa-bodyparser'
 import { koaSwagger } from 'koa2-swagger-ui'
 import swaggerSpec from '../config/swagger.config'
-import useRoutes from '../routers'
+import useRoutes, { BASE_PATH } from '../routers'
 import loggerHandle from '../middlewares/log4js.middleware'
 import responseHandle from './response-handle'
 import errorHandle from './error-handle'
 
 const app = new Koa()
+
+// 判断请求路径是否以 '/api' 开头，如果不是则添加前缀
+app.use(async (ctx, next) => {
+  if (!ctx.path.startsWith(BASE_PATH)) {
+    ctx.path = BASE_PATH + ctx.path
+  }
+  await next()
+})
 
 app.use(loggerHandle)
 app.use(cors())
